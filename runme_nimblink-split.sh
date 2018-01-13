d6495ef0885bff2ebd41ed3896ab1125a2bcbc41
Goetz van Rissenbeck <goetz@vanrissenbeck.de>
2018-01-13 19:40:13 +0100

    added separators to ease output reading

--- runme_nimblink-split.sh
+++ runme_nimblink-split.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -e
 newt build nrf51_boot
+echo "###################################"
 newt build nimblink
+echo "###################################"
 newt create-image nimblink 0.0.0
+echo "###################################"
 newt load nrf51_boot
+echo "###################################"
 newt load nimblink 
-newt run nimblink
+echo "###################################"
+newt run nimblink 0
